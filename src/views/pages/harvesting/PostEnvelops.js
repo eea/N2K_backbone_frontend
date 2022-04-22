66b51c0ec22d0efb913a8ce422e7292b5fb1bff3
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-04-22 10:27:13 +0200

    remove alert message

--- src/views/pages/harvesting/PostEnvelops.js
+++ src/views/pages/harvesting/PostEnvelops.js
@@ -6,7 +6,7 @@ import ConfigData from '../../../config.json';
 const PostEnvelops = ({version_id, country_code}) => {
     function postVersionIdHandler(version_id,country_code){
         //alert({versionId});
-        alert(version_id + "/" + country_code);
+        //alert(version_id + "/" + country_code);
 /*
         //var envIds= '[{    "VersionId": 123, "CountryCode": "ES"}, {    "VersionId": 1, "CountryCode": "AT"}]'; 
         //var harvested =[{"VersionId": 33, "CountryCode": "AT"}];
