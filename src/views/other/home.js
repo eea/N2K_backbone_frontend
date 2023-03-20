afe5c89f6e3e96dd4a3d54a6ddc213b77bc7f3c3
Amanda Rodriguez <arodriguez@bilbomatica.es>
2023-03-20 13:08:10 +0100

    NB-394

--- src/views/other/home.js
+++ src/views/other/home.js
@@ -31,10 +31,10 @@ const Home = (props) => {
           <CContainer fluid>
             <CRow>
               <CCol md={12} lg={12}>
-                <div className="page-title text-center p-3">
+                <div className="page-title text-center p-3 mt-5">
                   <h1 className="h1">Natura Change Manager</h1>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    This application is accessible only to authorised user, using their EU login. Please click on “Log In”.
                   </p>
                   <CButton color="secondary" href="/#/" onClick={(e)=>login(e)}>
                     <i className="fa-solid fa-arrow-right-to-bracket me-2"></i>Log In
