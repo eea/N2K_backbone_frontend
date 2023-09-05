faef41209860c590929dec4ace8313a5d5d65dcb
Amanda Rodriguez <arodriguez@bilbomatica.es>
2023-09-05 10:05:42 +0200

    Update country paramater

--- src/views/pages/sitechanges/TableRSPag.js
+++ src/views/pages/sitechanges/TableRSPag.js
@@ -626,6 +626,7 @@ const IndeterminateCheckbox = React.forwardRef(
       if(props.getRefresh()||(!isLoading && changesData!=="nodata" && Object.keys(changesData).length===0)){
         let promises=[];
         setIsLoading(true);
+        props.setLoadingSites(true);
         
         if(props.getRefresh()||(levelCountry==={})||(levelCountry.level!==props.level)||(levelCountry.country!==props.country)){
           props.setRefresh(props.status,false);  //For the referred status, data is updated
@@ -652,7 +653,10 @@ const IndeterminateCheckbox = React.forwardRef(
             }
           })
         )
-        Promise.all(promises).then(v=>setIsLoading(false));
+        Promise.all(promises).then(v=>{
+          setIsLoading(false);
+          props.setLoadingSites(false);
+        });
       }
     }
     
