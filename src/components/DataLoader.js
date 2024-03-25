0e774ee59d5f19d437f98e22fbba568407287b78
Amanda Rodriguez <arodriguez@bilbomatica.es>
2024-03-25 16:05:17 +0100

    Fix token expiration

--- src/components/DataLoader.js
+++ src/components/DataLoader.js
@@ -3,11 +3,14 @@ import ConfigData from '../config.json';
 export class DataLoader {
     constructor() {
         this.token = localStorage.getItem("token");
+        this.code = localStorage.getItem("code");
+        this.codeVerifier = localStorage.getItem("codeVerifier");
+        this.loginUrl = localStorage.getItem("loginUrl");
         if (!this.token) {
             if (window.location.href.includes("localhost")) {
                 this.token = ConfigData.token;
             }
-            else if (window.location.href !== window.location.origin + window.location.pathname + "#/") {
+            else if (window.location.href !== window.location.origin + window.location.pathname + "#/" && !this.code && !this.codeVerifier && !this.loginUrl) {
                 window.location.href = window.location.origin + window.location.pathname;
             }
         }
