9d04dcf96e8fb9cd1adcb3f46585013acb2ebf47
Joana R <joana.rente6@tuta.io>
2024-04-18 15:53:18 +0200

    sort country list by Country name instead of country code

--- src/views/pages/sitelineage/history.js
+++ src/views/pages/sitelineage/history.js
@@ -51,6 +51,7 @@ const Sitelineage = () => {
         let countriesList = [];
         if(data.Data.length > 0) {
           setLoadingCountries(false);
+          data.Data.sort((a,b) => a.Country > b.Country)
           for(let i in data.Data){
             countriesList.push({name:data.Data[i].Country,code:data.Data[i].Code});
           }
