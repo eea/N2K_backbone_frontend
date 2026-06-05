aa6670bcfc899e584f10eb64f33dd299bf7d52a5
Amanda Rodriguez <arodriguez@bilbomatica.es>
2026-06-05 14:58:11 +0200

    Separate documents and comments by year

--- src/views/pages/sitechanges/sharesite.js
+++ src/views/pages/sitechanges/sharesite.js
@@ -360,7 +360,18 @@ class ModalChanges extends Component {
       }
     }
     if (this.state.comments !== "noData") {
+      let lastYear = null;
       filteredComments.forEach(c => {
+        const dateString = c.EditedDate || c.Date;
+        const currentYear = dateString.substring(0, 4);
+        if (currentYear !== lastYear) {
+          cmts.push(
+            <div key={currentYear} className="year-separator">
+              <span>{currentYear}</span>
+            </div>
+          );
+          lastYear = currentYear;
+        }
         cmts.push(
           this.createCommentElement(c.Id, c.Comments, c.Date, c.Owner, c.Edited, c.EditedDate, c.EditedBy, target)
         )
@@ -420,7 +431,18 @@ class ModalChanges extends Component {
         filteredDocuments = this.state.documents?.filter(d => !d.Release)
     }
     if (this.state.documents !== "noData") {
+      let lastYear = null;
       filteredDocuments.forEach(d => {
+        const dateString = d.EditedDate || d.ImportDate;
+        const currentYear = dateString.substring(0, 4);
+        if (currentYear !== lastYear) {
+          docs.push(
+            <div key={currentYear} className="year-separator">
+              <span>{currentYear}</span>
+            </div>
+          );
+          lastYear = currentYear;
+        }
         const name = d.OriginalName ?? d.Path;
         docs.push(
           this.createDocumentElement(d.Id, name, d.ImportDate, d.Username, d.Comment, d.Edited, d.EditedDate, d.EditedBy, target)
